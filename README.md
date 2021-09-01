48d9a6eef874b29e49513a97b138d01f3536d118
Ayman Adil <adilayman53@gmail.com>
2021-09-01 15:55:50 +0200

    small improvements

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-
 # [PHP] SQL DATABASE USING PDO
-*By Ayman Adil (Last source code update: 20/09/2020)*
 
+_By Ayman Adil (Last source code update: 20/09/2020)_
 
 ## Table of contents
+
 - [Description](#description)
 - [SELECT clause](#select-clause)
 - [WHERE clause](#where-clause)
@@ -20,222 +20,224 @@
 - [PDO methods](#pdo-methods)
 
 ### Description
+
 The purpose of this program is to facilitate the creation of SQL queries on PHP.
 
 First create the query with <tt>select()</tt>, <tt>insert()</tt>, <tt>update()</tt>, <tt>where()</tt>, ... methods then prepare and execute it with <tt>[result()](#get-results)</tt> method.
-
 
 ### SELECT clause
 
 #### Select all columns
 
-~~~~php
+```php
 // [SQL] SELECT * FROM Users
 $db->select('*', 'Users');
 $result = $db->result();
-~~~~
+```
 
 #### Select a single column
 
-~~~~php
+```php
 // [SQL] SELECT first_name FROM Users
 $db->select('first_name', 'Users');
 $result = $db->result('fetch'); // i.e using PDOStatement::fetch
-~~~~
+```
 
 #### Select multiple columns
 
-~~~~php
+```php
 // [SQL] SELECT first_name, last_name FROM Users
 $db->select(['first_name', 'last_name'], 'Users');
 $result = $db->result();
-~~~~
+```
 
 ### WHERE clause
 
 #### AND/OR OPERATORS
-~~~~php
+
+```php
 // [SQL] SELECT * FROM Users WHERE id > 10 AND birthdate = '2012-12-12'
 $db->select('*', 'Users')->where(['id >' => 10, 'birthdate =' => '2012-12-12']);
 $result = $db->result();
-~~~~
-~~~~php
+```
+
+```php
 // [SQL] SELECT * FROM Users WHERE id > 10 OR birthdate = '2012-12-12'
 $db->select('*', 'Users')->where(['id >' => 10, 'birthdate =' => '2012-12-12'], 'OR');
 $result = $db->result();
-~~~~
+```
 
 #### IN/NOT IN OPERATOR
 
-~~~~php
+```php
 // [SQL] SELECT * FROM Users WHERE birthdate IN ('2011-11-11', '2012-12-12')
 $db->select('*', 'Users')->where(['birthdate IN' => ['2011-11-11', '2012-12-12']]);
 $result = $db->result();
-~~~~
+```
 
 #### BETWEEN/NOT BETWEEN OPERATOR
 
-~~~~php
+```php
 // [SQL] SELECT * FROM Users WHERE id BETWEEN 51 AND 100
 $db->select('*', 'Users')->where(['id BETWEEN' => ['51', '100']]);
 $result = $db->result();
-~~~~
+```
 
 ### GROUP BY clause
 
 #### Group by one column
 
-~~~~php
+```php
 // [SQL] SELECT id, SUM('orderPrices') FROM 'Orders' GROUP BY id
 $db->select(['id', 'SUM(orderPrices)'], 'Orders')->groupBy('id');
 $result = $db->result();
-~~~~
+```
 
 #### Group by multiple columns
 
-~~~~php
+```php
 // [SQL] SELECT first_name, last_name, SUM('orderPrices') FROM 'Orders' GROUP BY first_name, last_name
 $db->select(['first_name', 'last_name', 'SUM(orderPrices)'], 'Orders')->groupBy(['first_name', 'last_name']);
 $result = $db->result();
-~~~~
+```
 
 ### ORDER BY clause
 
 #### Order by one column
 
-~~~~php
+```php
 // [SQL] SELECT id, first_name FROM 'Users' ORDER BY id DESC
 $db->select(['id', 'first_name'], 'Users')->orderBy('id DESC');
 $result = $db->result();
-~~~~
+```
 
 #### Order by multiple columns
 
-~~~~php
+```php
 // [SQL] SELECT id, first_name, birthday FROM 'Users' ORDER BY id DESC, first_name
 $db->select(['id', 'first_name'], 'Users')->orderBy(['id DESC', 'first_name']);
 $result = $db->result();
-~~~~
+```
 
 ### AS (alias) keyword
 
 #### Aliases on columns
 
-~~~~php
+```php
 // [SQL] SELECT id AS identity, first_name AS first_name FROM 'Users'
 $db->select(['id AS identity', 'first_name AS firstName'], 'Users');
 $result = $db->result();
-~~~~
+```
 
 #### Alias on table
 
-~~~~php
+```php
 // [SQL] SELECT * FROM 'Users' AS UsersInformation
 $db->select('*', 'Users AS UsersInformation');
 $result = $db->result();
-~~~~
+```
 
 ### INSERT clause
 
 #### Insert only values
 
-~~~~php
+```php
 // [SQL] INSERT INTO Users VALUES (5, 'toto', 'moto', '10-10-2010');
 $db->insertInto('Users', [5, 'toto', 'moto', '10-10-2010']);
 $result = $db->result();
-~~~~
+```
 
 #### Insert values in only specified columns
 
-~~~~php
+```php
 // [SQL] INSERT INTO Users ('first_name', 'last_name') VALUES ('toto', 'moto');
 $db->insertInto('Users', ['first_name', 'last_name'], ['toto', 'moto']);
 $result = $db->result();
-~~~~
+```
 
 ### UPDATE clause
 
 #### Update all rows
 
-~~~~php
+```php
 // [SQL] UPDATE 'Users' SET 'first_name' = 'toto';
 $db->update('Users', ['first_name' => 'toto']);
 $result = $db->result();
-~~~~
+```
 
 #### Update a specified row
 
-~~~~php
+```php
 // [SQL] UPDATE 'Users' SET 'first_name' = 'toto' WHERE id = 10;
 $db->update('Users', ['first_name' => 'toto'])->where(['id =' => 10]);
 $result = $db->result();
-~~~~
+```
 
 ### UNION/UNION ALL clauses
 
 #### UNION clause
 
-~~~~php
+```php
 // [SQL] SELECT * FROM 'Orders1' UNION SELECT * FROM 'Orders2'
 $db->select('*', 'Orders1')->unionSelect('*', 'Orders2');
 $result = $db->result();
-~~~~
+```
 
 #### UNION ALL clause
 
-~~~~php
+```php
 // [SQL] SELECT * FROM 'Orders1' UNION ALL SELECT * FROM 'Orders2'
 $db->select('*', 'Orders1')->unionAllSelect('*', 'Orders2');
 $result = $db->result();
-~~~~
+```
 
 ### NATURAL JOIN clause
 
-~~~~php
+```php
 // [SQL] SELECT * FROM Users NATURAL JOIN Country
 $db->select('*', 'Users')->naturalJoin('Country');
 $result = $db->result();
-~~~~
+```
 
 ### DELETE clause
 
 #### Delete all rows
 
-~~~~php
+```php
 // [SQL] DELETE FROM 'Users'
 $db->delete('Users');
 $result = $db->result();
-~~~~
+```
 
 #### Delete specific rows
 
-~~~~php
+```php
 // [SQL] DELETE FROM 'Users' WHERE id = 10
 $db->delete('Users')->where(['id =' => 10]);
 $result = $db->result();
-~~~~
+```
 
 ### TRUNCATE TABLE clause
 
-~~~~php
+```php
 // [SQL] TRUNCATE TABLE 'Users'
 $db->truncate('Users');
 $result = $db->result();
-~~~~
+```
 
 ### LIMIT clause
 
-~~~~php
+```php
 // [SQL] SELECT * FROM Users LIMIT 2
 $db->select('*', 'Users')->limit(2);
 $result = $db->result();
-~~~~
+```
 
-~~~~php
+```php
 // [SQL] SELECT * FROM Users LIMIT 3, 2
 $db->select('*', 'Users')->limit(3, 2);
 $result = $db->result();
-~~~~
+```
 
 ### Get results
 
--- src/Database.php
+++ src/Database.php
@@ -8,294 +8,280 @@
 
 class Database extends PDO
 {
-	private const NO_MODE = 0;
-	private const EXTRACT_MODE = 1;
-	private const INSERT_MODE = 2;
-
-	private $query = '';
-	private $values = [];
-	private $mode = self::NO_MODE;
-
-	public function __construct($dbname, $host, $username, $password)
-	{
-		try {
-			$dsn = "mysql:dbname=$dbname;host=$host";
-			parent::__construct($dsn, $username, $password);
-			parent::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			parent::setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-		} catch (PDOException $e) {
-			throw new Exception('Connection failed: ' . $e->getMessage());
-		}
-	}
-
-	public function select($columns, $table)
-	{
-		if ($this->mode === self::NO_MODE) {
-			$this->mode = self::EXTRACT_MODE;
-		}
-
-		$this->query .= 'SELECT ';
-
-		if (is_array($columns)) {
-			$this->query .= implode(',', $columns);
-		} else {
-			$this->query .= $columns;
-		}
-
-		$this->query .= " FROM $table";
-
-		return $this;
-	}
-
-	public function where($conditions, $logic = 'AND')
-	{
-		if ($this->mode === self::NO_MODE) {
-			throw new Exception("WHERE clause cannot be at the beginning of the query.");
-		}
-
-		$this->query .= ' WHERE';
-
-		$condSize = count($conditions);
-
-		foreach ($conditions as $key => $value) {
-			if (is_array($value)) {
-				$exploded = explode(' ', $key, 3);
-				$length = count($exploded);
-
-				if (!strcmp($exploded[$length - 1], 'BETWEEN')) {
-					$this->query .= " ($key ? AND ?)";
-					$this->values[] = $value[0];
-					$this->values[] = $value[1];
-				} elseif (!strcmp($exploded[$length - 1], 'IN')) {
-					$this->query .= " $key ($this->replace($value))";
-				} else {
-					$this->query .= ' ' . $key;
-
-					foreach ($value as $k => $v) {
-						$this->values[] = $v;
-					}
-				}
-
-				if (--$condSize > 0) {
-					$this->query .= ' ' . $logic;
-				}
-			} else {
-				$this->query .= " $key";
-
-				if ($value === NULL) {
-					$this->query .= " NULL";
-				} else {
-					$this->query .= " ?";
-
-					if (--$condSize > 0) {
-						$this->query .= ' ' . $logic;
-					}
-
-					$this->values[] = $value;
-				}
-			}
-		}
-
-		return $this;
-	}
-
-	public function groupBy($columns)
-	{
-		if ($this->mode === self::NO_MODE) {
-			throw new Exception("GROUP BY clause cannot be at the beginning of the query.");
-		}
-
-		$this->query .= ' GROUP BY ';
-
-		if (is_array($columns)) {
-			$this->query .= implode(',', $columns);
-		} else {
-			$this->query .= $columns;
-		}
+    private const NO_MODE = 0;
+    private const EXTRACT_MODE = 1;
+    private const INSERT_MODE = 2;
+
+    private $query = '';
+    private $values = [];
+    private $mode = self::NO_MODE;
+
+    public function __construct($dbname, $host, $username, $password)
+    {
+        try {
+            $dsn = "mysql:dbname=$dbname;host=$host";
+            parent::__construct($dsn, $username, $password);
+            parent::setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
+            parent::setAttribute(
+                PDO::ATTR_DEFAULT_FETCH_MODE,
+                PDO::FETCH_ASSOC
+            );
+        } catch (PDOException $e) {
+            throw new Exception('Connection failed: ' . $e->getMessage());
+        }
+    }
+
+    public function select($columns, $table)
+    {
+        if ($this->mode === self::NO_MODE)
+            $this->mode = self::EXTRACT_MODE;
+
+        $this->query .= 'SELECT ';
+
+        if (is_array($columns))
+            $this->query .= implode(',', $columns);
+        else
+            $this->query .= $columns;
+
+        $this->query .= " FROM $table";
+
+        return $this;
+    }
+
+    public function where($conditions, $logic = 'AND')
+    {
+        if ($this->mode === self::NO_MODE)
+            throw new Exception("WHERE clause cannot be at the beginning of the query.");
+
+        $this->query .= ' WHERE';
+
+        $condSize = count($conditions);
+
+        foreach ($conditions as $key => $value) {
+            if (is_array($value)) {
+                $exploded = explode(' ', $key, 3);
+                $length = count($exploded);
+
+                if (!strcmp($exploded[$length - 1], 'BETWEEN')) {
+                    $this->query .= " ($key ? AND ?)";
+                    $this->values[] = $value[0];
+                    $this->values[] = $value[1];
+                } elseif (!strcmp($exploded[$length - 1], 'IN')) {
+                    $this->query .= " $key ($this->replace($value))";
+                } else {
+                    $this->query .= ' ' . $key;
+
+                    foreach ($value as $k => $v) {
+                        $this->values[] = $v;
+                    }
+                }
+
+                if (--$condSize > 0) {
+                    $this->query .= ' ' . $logic;
+                }
+            } else {
+                $this->query .= " $key";
+
+                if ($value === NULL) {
+                    $this->query .= " NULL";
+                } else {
+                    $this->query .= " ?";
+
+                    if (--$condSize > 0) {
+                        $this->query .= ' ' . $logic;
+                    }
+
+                    $this->values[] = $value;
+                }
+            }
+        }
+
+        return $this;
+    }
+
+    public function groupBy($columns)
+    {
+        if ($this->mode === self::NO_MODE)
+            throw new Exception("GROUP BY clause cannot be at the beginning of the query.");
+
+        $this->query .= ' GROUP BY ';
+
+        if (is_array($columns))
+            $this->query .= implode(',', $columns);
+        else
+            $this->query .= $columns;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function orderBy($columns)
-	{
-		if ($this->mode === self::NO_MODE) {
-			throw new Exception("ORDER BY clause cannot be at the beginning of the query.");
-		}
+    public function orderBy($columns)
+    {
+        if ($this->mode === self::NO_MODE)
+            throw new Exception("ORDER BY clause cannot be at the beginning of the query.");
 
-		$this->query .= ' ORDER BY ';
+        $this->query .= ' ORDER BY ';
 
-		if (is_array($columns)) {
-			$this->query .= implode(',', $columns);
-		} else {
-			$this->query .= $columns;
-		}
+        if (is_array($columns))
+            $this->query .= implode(',', $columns);
+        else
+            $this->query .= $columns;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function update($table, $columns)
-	{
-		if ($this->mode === self::NO_MODE) {
-			$this->mode = self::INSERT_MODE;
-		}
+    public function update($table, $columns)
+    {
+        if ($this->mode === self::NO_MODE)
+            $this->mode = self::INSERT_MODE;
 
-		$this->query .= " UPDATE $table SET";
+        $this->query .= " UPDATE $table SET";
+        $this->separate($columns);
 
-		$this->seperate($columns);
+        return $this;
+    }
 
-		return $this;
-	}
+    public function unionSelect($columns, $table)
+    {
+        if ($this->mode === self::NO_MODE)
+            throw new Exception("UNION clause cannot be at the beginning of the query.");
 
-	public function unionSelect($columns, $table)
-	{
-		if ($this->mode === self::NO_MODE) {
-			throw new Exception("UNION clause cannot be at the beginning of the query.");
-		}
+        $this->query .= ' UNION ';
 
-		$this->query .= ' UNION ';
+        return $this->select($columns, $table);
+    }
 
-		return $this->select($columns, $table);
-	}
+    public function unionAllSelect($columns, $table)
+    {
+        if ($this->mode === self::NO_MODE)
+            throw new Exception("UNION ALL clause cannot be at the beginning of the query.");
 
-	public function unionAllSelect($columns, $table)
-	{
-		if ($this->mode === self::NO_MODE) {
-			throw new Exception("UNION ALL clause cannot be at the beginning of the query.");
-		}
+        $this->query .= ' UNION ALL ';
 
-		$this->query .= ' UNION ALL ';
+        return $this->select($columns, $table);
+    }
 
-		return $this->select($columns, $table);
-	}
+    public function insertInto($table, $arg1, $arg2 = NULL)
+    {
+        if ($this->mode === self::NO_MODE)
+            $this->mode = self::INSERT_MODE;
 
-	public function insertInto($table, $arg1, $arg2 = NULL)
-	{
-		if ($this->mode === self::NO_MODE) {
-			$this->mode = self::INSERT_MODE;
-		}
+        $this->query .= "INSERT INTO $table";
 
-		$this->query .= "INSERT INTO $table";
+        if ($arg2 !== NULL) {
+            $this->query .= '(' . implode(',', $arg1) . ')';
+            $this->query .= ' VALUES ';
+            $this->replace($arg2);
+        } else {
+            $this->query .= ' VALUES ';
+            $this->replace($arg1);
+        }
 
-		if ($arg2 !== NULL) {
-			$this->query .= '(' . implode(',', $arg1) . ')';
-			$this->query .= ' VALUES ';
-			$this->replace($arg2);
-		} else {
-			$this->query .= ' VALUES ';
-			$this->replace($arg1);
-		}
-
-		return $this;
-	}
-
-	public function delete($table)
-	{
-		if ($this->mode !== self::NO_MODE) {
-			throw new Exception("The initial query has to be empty");
-		}
-
-		$this->mode = self::INSERT_MODE;
-
-		$this->query .= "DELETE FROM $table";
-
-		return $this;
-	}
-
-	public function truncate($table)
-	{
-		if ($this->mode !== self::NO_MODE) {
-			throw new Exception("The initial query has to be empty");
-		}
-
-		$this->mode = self::INSERT_MODE;
-
-		$this->query .= "TRUNCATE $table";
-
-		return $this;
-	}
-
-	public function limit($arg1, $arg2 = NULL)
-	{
-		$this->query .= "LIMIT $arg1";
-
-		if ($arg2 !== NULL) {
-			$this->query .= ", $arg2";
-		}
-		return;
-	}
-
-	public function result($fetchMode = 'fetch')
-	{
-		$result = NULL;
-
-		if ($this->mode === self::EXTRACT_MODE) {
-			$result = $this->extraction($this->query, $this->values, $fetchMode);
-		} elseif ($this->mode === self::INSERT_MODE) {
-			$result = $this->insertion($this->query, $this->values);
-		}
-
-		$this->reset();
-
-		return $result;
-	}
-
-	public function insertion($query, array $params = NULL)
-	{
-		return $this->prepare($query)->execute($params);
-	}
-
-	public function extraction($query, $params = NULL, $fetchMode = 'fetchAll')
-	{
-		$stmt = $this->prepare($query);
-
-		$stmt->execute($params);
-
-		if (!method_exists($stmt, $fetchMode)) {
-			throw new Exception("Undefined method PDOStatement::$fetchMode()");
-		}
-
-		return $stmt->$fetchMode();
-	}
-
-	public function reset()
-	{
-		$this->query = '';
-		$this->values = [];
-		$this->mode = self::NO_MODE;
-		return;
-	}
-
-	private function replace($array)
-	{
-		$this->query .= '(';
-		$count = count($array);
-
-		for ($i = 0; $i < $count; ++$i) {
-			$this->query .= '?';
-
-			if ($i < $count - 1) {
-				$this->query .= ',';
-			}
-			$this->values[] .= $array[$i];
-		}
-		$this->query .= ')';
-		return;
-	}
-
-	private function seperate($array, $seperator = 'AND')
-	{
-		$count = count($array);
-
-		foreach ($array as $key => $value) {
-			$this->query .= " $key = ?";
-
-			if (--$count > 0) {
-				$this->query .= ' ' . $seperator;
-			}
-
-			$this->values[] = $value;
-		}
-
-		return;
-	}
+        return $this;
+    }
+
+    public function delete($table)
+    {
+        if ($this->mode !== self::NO_MODE)
+            throw new Exception("The initial query has to be empty");
+
+        $this->mode = self::INSERT_MODE;
+        $this->query .= "DELETE FROM $table";
+
+        return $this;
+    }
+
+    public function truncate($table)
+    {
+        if ($this->mode !== self::NO_MODE)
+            throw new Exception("The initial query has to be empty");
+
+        $this->mode = self::INSERT_MODE;
+        $this->query .= "TRUNCATE $table";
+
+        return $this;
+    }
+
+    public function limit($arg1, $arg2 = NULL)
+    {
+        $this->query .= "LIMIT $arg1";
+
+        if ($arg2 !== NULL)
+            $this->query .= ", $arg2";
+    }
+
+    public function result($fetchMode = 'fetch')
+    {
+        $result = NULL;
+
+        if ($this->mode === self::EXTRACT_MODE)
+            $result = $this->extraction(
+                $this->query,
+                $this->values,
+                $fetchMode
+            );
+        elseif ($this->mode === self::INSERT_MODE)
+            $result = $this->insertion($this->query, $this->values);
+
+        $this->reset();
+
+        return $result;
+    }
+
+    public function insertion($query, array $params = NULL)
+    {
+        return $this->prepare($query)->execute($params);
+    }
+
+    public function extraction($query, $params = NULL, $fetchMode = 'fetchAll')
+    {
+        $stmt = $this->prepare($query);
+
+        $stmt->execute($params);
+
+        if (!method_exists($stmt, $fetchMode))
+            throw new Exception("Undefined method PDOStatement::$fetchMode()");
+
+        return $stmt->$fetchMode();
+    }
+
+    public function reset()
+    {
+        $this->query = '';
+        $this->values = [];
+        $this->mode = self::NO_MODE;
+    }
+
+    private function replace($array)
+    {
+        $this->query .= '(';
+        $count = count($array);
+
+        for ($i = 0; $i < $count; ++$i) {
+            $this->query .= '?';
+
+            if ($i < $count - 1)
+                $this->query .= ',';
+
+            $this->values[] .= $array[$i];
+        }
+        $this->query .= ')';
+    }
+
+    private function separate($array, $separator = 'AND')
+    {
+        $count = count($array);
+
+        foreach ($array as $key => $value) {
+            $this->query .= " $key = ?";
+
+            if (--$count > 0) {
+                $this->query .= ' ' . $separator;
+            }
+
+            $this->values[] = $value;
+        }
+    }
 }
